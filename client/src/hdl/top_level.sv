8fc93ac09da313ea49ed1670933a7d9940666ac2
Yohan Guyomard <yohang@mit.edu>
2024-11-02 22:31:51 -0400

    Add Verilog boilerplate

---
+++ client/src/hdl/top_level.sv
@@ -0,0 +1,14 @@
+`default_nettype none
+
+module top_level(
+    input wire clk_100mhz,
+    
+    input wire [15:0]   sw,
+    input wire [3:0]    btn
+);
+
+// wow, so empty
+
+endmodule
+
+`default_nettype wire
