b8f1d38f7e134f7af5312a443f18e4c3443e7786
Yohan Guyomard <yohang@mit.edu>
2024-11-08 02:59:30 -0500

    Proof of concept with querying blocks AND serial

--- server/plugin/src/lib.rs
+++ server/plugin/src/lib.rs
@@ -1,5 +1,5 @@
 
-use quill::{events::BlockPlacementEvent, Game, Plugin};
+use quill::{BlockPosition, Game, Plugin, Position};
 
 #[quill::plugin]
 pub struct BlockPlace;
@@ -14,7 +14,27 @@ impl Plugin for BlockPlace {
 }
 
 fn system(_plugin: &mut BlockPlace, game: &mut Game) {
-    for (_entity, _event) in game.query::<&BlockPlacementEvent>() {
-        println!("A client has placed a block!");
+    // WARN: just like how sending absolutely everything (block-data-wise) is too slow,
+    // simply iterating over all those blocks is *also* too slow. we should only be querying the
+    // blocks we need to send over serial
+    for (_, pos) in game.query::<&Position>() {
+        let pos = pos.block();
+        let mut tally = 0;
+        for z in -64..64 {
+            for y in -64..64 {
+                for x in -64..64 {
+                    let Ok(_block) = game.block(pos + BlockPosition::new(x, y, z)) else {
+                        continue;
+                    };
+                    tally += 1;
+                }
+            }
+        }
+        println!("Just iterated the {tally} blocks around {pos:?}!");
+    }
+
+    let ports = serialport::available_ports().expect("No ports found!");
+    for p in ports {
+        println!("{}", p.port_name);
     }
 }
