37cbb0d52d8ec294ea1eac8f849f7c82b653d652
Yohan Guyomard <yohang@mit.edu>
2024-11-27 14:58:17 -0500

    Add mock cache

---
+++ sw/src/cache/mock.rs
@@ -0,0 +1,40 @@
+use crate::{block::Block, math::Vec3i};
+
+/// Unrealistic cache with instant access, for testing
+pub struct MockCache {
+    chunk: &'static [Block]
+}
+
+static CHUNK: &[u8] = include_bytes!("chunk.bin");
+
+impl Default for MockCache {
+    fn default() -> Self {
+        assert!(size_of::<u8>() == size_of::<Block>());
+        Self {
+            // SAFETY:
+            // Underlying representation [Block] is just `u8`
+            chunk: unsafe { std::slice::from_raw_parts(CHUNK.as_ptr() as _, CHUNK.len()) }
+        }
+    }
+}
+
+impl MockCache {
+    /// Size of the cache in one dimension
+    pub const SIZE: usize = 128;
+
+    pub fn query(&mut self, idx: Vec3i) -> Option<Block> {
+        const HALF_CHUNK_SIZE: i32 = (MockCache::SIZE as i32) / 2;
+        
+        // Re-center position in chunk
+        let x: usize = (idx.x + HALF_CHUNK_SIZE).try_into().ok()?;
+        let y: usize = (idx.y + HALF_CHUNK_SIZE).try_into().ok()?;
+        let z: usize = (idx.z + HALF_CHUNK_SIZE).try_into().ok()?;
+
+        // Out of bounds
+        if x > Self::SIZE || y > Self::SIZE || z > Self::SIZE {
+            return None;
+        }
+
+        Some(self.chunk[Self::SIZE * (Self::SIZE * z + y) + x])
+    }
+}
--- sw/src/cache/mod.rs
+++ sw/src/cache/mod.rs
@@ -1,5 +1,7 @@
 mod l1;
 mod l2;
+mod mock;
 
 pub use l1::L1Cache;
 pub use l2::L2Cache;
+pub use mock::MockCache;
