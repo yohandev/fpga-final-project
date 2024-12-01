6a5dd789c91adda990e2d5d9c86f8b0581054aab
Yohan Guyomard <yohang@mit.edu>
2024-11-30 21:41:46 -0500

    Add `fixed` type and `fixed_inv_sqrt` module

---
+++ hw/src/sim/test_fixed.py
@@ -0,0 +1,127 @@
+import cocotb
+import os
+import sys
+from pathlib import Path
+from cocotb.clock import Clock
+from cocotb.binary import BinaryValue, BinaryRepresentation
+from cocotb.triggers import ClockCycles, FallingEdge
+from cocotb.runner import get_runner
+from random import random
+from math import sqrt
+
+
+D = 15
+FIXED_MAX = (1 << 31) - 1;
+FIXED_MIN = -(1 << 31)
+
+def fixed(f): return int(f * (1 << D))
+def f32(fx): return float(fx / (1 << D))
+
+def fixed_mul(a, b): return int((a * b) >> D)
+
+def leading_zeros(n):
+    for i in reversed(range(32)):
+        if n & (1 << i):
+            return 31 - i
+    return 32
+
+def fixed_inv_sqrt(fx):
+    def lut(lz):
+        if lz == 31:
+            return fixed(1 / sqrt(f32(0b1)))
+        elif 0 <= lz <= 30:
+            return fixed(1 / sqrt(f32(0b11 << (30 - lz))))
+    
+    # First iteration (LUT)
+    iter0 = lut(leading_zeros(fx) - 1)
+    
+    # Second iteration (Newton)
+    iter1 = fixed_mul(iter0, fixed(1.5) - fixed_mul(fx >> 1, fixed_mul(iter0, iter0)))
+
+    # Third iteration (Newton)
+    iter2 = fixed_mul(iter1, fixed(1.5) - fixed_mul(fx >> 1, fixed_mul(iter1, iter1)))
+
+    return iter2
+
+def twos_complement(n):
+    return BinaryValue(n, 32, False, BinaryRepresentation.TWOS_COMPLEMENT).integer
+
+
+async def generic_test(dut, op, output, *, positive=False):
+    for _ in range(1000):
+        await FallingEdge(dut.clk_in)
+        
+        a = fixed((random() - 0.5) * f32(FIXED_MAX))
+        b = fixed((random() - 0.5) * f32(FIXED_MAX))
+
+        if positive:
+            a = abs(a)
+            b = abs(b)
+
+        out = op((a, b))
+
+        # Ignore overflows
+        if not (FIXED_MIN <= out <= FIXED_MAX):
+            continue
+
+        dut.a.value = twos_complement(a)
+        dut.b.value = twos_complement(b)
+
+        await ClockCycles(dut.clk_in, 1, rising=False)
+
+        assert out == output(dut).value.signed_integer
+
+@cocotb.test()
+async def test_expr(dut):
+    cocotb.start_soon(Clock(dut.clk_in, 10, units="ns").start())
+
+    print("Testing addition...")
+    await generic_test(dut, lambda ab: ab[0] + ab[1], lambda d: d.add)
+
+    print("Testing subtraction...")
+    await generic_test(dut, lambda ab: ab[0] - ab[1], lambda d: d.sub)
+
+    print("Testing multiplication...")
+    await generic_test(dut, lambda ab: fixed_mul(*ab), lambda d: d.mul)
+
+    print("Testing mixed expressions...")
+    await generic_test(dut, lambda ab: fixed_mul((fixed_mul(ab[0], ab[1]) + (ab[1] - ab[0])), (ab[0] - ab[1])), lambda d: d.mul)
+    
+    print("Testing inverse square root...")
+    await generic_test(dut, lambda ab: fixed_inv_sqrt(ab[0]), lambda d: d.inv_sqrt, positive=True)
+
+
+def is_runner():
+    """Image Sprite Tester."""
+    hdl_toplevel_lang = os.getenv("HDL_TOPLEVEL_LANG", "verilog")
+    sim = os.getenv("SIM", "icarus")
+    proj_path = Path(__file__).resolve().parent.parent
+    sys.path.append(str(proj_path / "sim" / "model"))
+    print(proj_path)
+    sources = [proj_path / "hdl" / "fixed.sv"]
+    includes = [proj_path / "hdl"]
+    build_test_args = ["-Wall"]
+    parameters = {}
+    sys.path.append(str(proj_path / "sim"))
+    runner = get_runner(sim)
+    runner.build(
+        sources=sources,
+        includes=includes,
+        hdl_toplevel="fixed_testbench",
+        always=True,
+        build_args=build_test_args,
+        parameters=parameters,
+        timescale = ('1ns','1ps'),
+        waves=True,
+        build_dir=(proj_path.parent / "sim_build")
+    )
+    run_test_args = []
+    runner.test(
+        hdl_toplevel="fixed_testbench",
+        test_module="test_fixed",
+        test_args=run_test_args,
+        waves=True
+    )
+
+if __name__ == "__main__":
+    is_runner()
--- sw/py/fixed_inv_sqrt.py
+++ sw/py/fixed_inv_sqrt.py
@@ -16,15 +16,15 @@ def leading_zeros(n):
             return 31 - i
     return 32
 
+def lut(lz):
+    if lz == 31:
+        return fixed(1 / sqrt(f32(0b1)))
+    elif 0 <= lz <= 30:
+        return fixed(1 / sqrt(f32(0b11 << (30 - lz))))
+
 def fixed_inv_sqrt(fx):
     # First iteration (LUT)
-    iter0 = 0
-    for i in range(31):
-        if leading_zeros(fx) == i:
-            iter0 = fixed(1 / sqrt(f32(0b11 << (30 - i))))
-            break
-    if leading_zeros(fx) == 31:
-        iter0 = fixed(1 / sqrt(f32(0b1)))
+    iter0 = lut(leading_zeros(fx))
     
     # Second iteration (Newton)
     iter1 = fixed_mul(iter0, fixed(1.5) - fixed_mul(fx >> 1, fixed_mul(iter0, iter0)))
@@ -42,7 +42,14 @@ def err(f):
     
     return abs(actual - predicted) / actual
 
-x = np.arange(1, 100000, 0.1)
+
+# Generate SystemVerilog LUT
+for i in range(1, 31):
+    print(f"32'sb?{'0' * i}1{'?' * (30-i)}: iter0 = 32'sh{hex(lut(i))[2:]};")
+print(f"default: iter0 = 32'sh{hex(lut(31))[2:]};")
+
+# Generate error plot
+x = np.arange(0.001, 60000, 0.1)
 y = [100 * err(i) for i in x]
 
 fig, ax = plt.subplots()
@@ -50,5 +57,7 @@ fig, ax = plt.subplots()
 ax.plot(x, y)
 ax.set(xlabel="x", ylabel="% Error", title="1/sqrt(x)")
 ax.grid()
+
+print(f"Average error was {np.average(y)}")
 
 plt.show()
