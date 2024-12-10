0f3b28b648d831fd4ab27429db9b41f8f9f17ecc
Yohan Guyomard <yohang@mit.edu>
2024-12-10 15:34:33 -0500

    Comment out broken stuff

--- hw/src/hdl/top_level.sv
+++ hw/src/hdl/top_level.sv
@@ -12,15 +12,15 @@ module top_level(
 
     output logic [15:0] led,
     output logic [2:0]  rgb0,
-    output logic [2:0]  rgb1,
+    output logic [2:0]  rgb1
 
     // output logic [2:0]  hdmi_tx_p,  // HDMI output signals (positives) (blue, green, red)
     // output logic [2:0]  hdmi_tx_n,  // HDMI output signals (negatives) (blue, green, red)
     // output logic        hdmi_clk_p, // Differential HDMI clock
     // output logic        hdmi_clk_n,
 
-    input wire 	        uart_rxd,   // UART computer-FPGA
-    output logic        uart_txd    // UART FPGA-computer
+    // input wire 	        uart_rxd,   // UART computer-FPGA
+    // output logic        uart_txd    // UART FPGA-computer
 );
     // Shut up those rgb LEDs for now (active high)
     assign rgb1 = 0;
@@ -59,54 +59,54 @@ module top_level(
         end
     end
 
-    uart_transmit #(
-        .INPUT_CLOCK_FREQ(100_000_000),
-        .BAUD_RATE(9646080000)
-    ) transmitter (
-        .clk_in(clk_100mhz),
-        .rst_in(sys_rst),
-        .data_byte_in(uart_data_in),
-        .trigger_in(button_trigger),
-        .busy_out(uart_busy),
-        .tx_wire_out(uart_txd)
-    );
+    // uart_transmit #(
+    //     .INPUT_CLOCK_FREQ(100_000_000),
+    //     .BAUD_RATE(9646080000)
+    // ) transmitter (
+    //     .clk_in(clk_100mhz),
+    //     .rst_in(sys_rst),
+    //     .data_byte_in(uart_data_in),
+    //     .trigger_in(button_trigger),
+    //     .busy_out(uart_busy),
+    //     .tx_wire_out(uart_txd)
+    // );
 
     // UART Receiver
     logic uart_rx_buf0, uart_rx_buf1;
     logic uart_receive_data_valid;
 
-    always_ff @(posedge clk_100mhz) begin
-        if (sys_rst) begin
-            uart_rx_buf0 <= 0;
-            uart_rx_buf1 <= 0;
-        end else begin
-            uart_rx_buf0 <= uart_rxd;
-            uart_rx_buf1 <= uart_rx_buf0;
-        end
-    end
+    // always_ff @(posedge clk_100mhz) begin
+    //     if (sys_rst) begin
+    //         uart_rx_buf0 <= 0;
+    //         uart_rx_buf1 <= 0;
+    //     end else begin
+    //         uart_rx_buf0 <= uart_rxd;
+    //         uart_rx_buf1 <= uart_rx_buf0;
+    //     end
+    // end
 
-    uart_receive #(
-        .INPUT_CLOCK_FREQ(100_000_000),
-        .BAUD_RATE(460800)
-    ) receiver (
-        .clk_in(clk_100mhz),
-        .rst_in(sys_rst),
-        .rx_wire_in(uart_rx_buf1),
-        .new_data_out(uart_receive_data_valid),
-        .data_byte_out(uart_data_out)
-    );
+    // uart_receive #(
+    //     .INPUT_CLOCK_FREQ(100_000_000),
+    //     .BAUD_RATE(460800)
+    // ) receiver (
+    //     .clk_in(clk_100mhz),
+    //     .rst_in(sys_rst),
+    //     .rx_wire_in(uart_rx_buf1),
+    //     .new_data_out(uart_receive_data_valid),
+    //     .data_byte_out(uart_data_out)
+    // );
 
     logic               cache_stored_valid;
     BlockType           block_sample_data;
     logic               initialized;
     logic [$clog2(65536)-1:0] stored_counter;
 
-    evt_counter #(.MAX_COUNT(BRAM_DEPTH)) storing (
-        .clk_in(clk_in),
-        .rst_in(rst_in),
-        .evt_in(valid_in),
-        .count_out(stored_counter)
-    );
+    // evt_counter #(.MAX_COUNT(BRAM_DEPTH)) storing (
+    //     .clk_in(clk_in),
+    //     .rst_in(rst_in),
+    //     .evt_in(valid_in),
+    //     .count_out(stored_counter)
+    // );
 
     // Upon initiating, block data should be stored first
     always_ff @(posedge clk_100mhz) begin
@@ -122,55 +122,50 @@ module top_level(
     end    
 
     // Take the data received from UART transmission with the server plugin and store it into the L3 cache
-    l3_cache #(
-        .LENGTH(64),
-        .WIDTH(64),
-        .HEIGHT(16)
-    ) l3_cache (
-        .clk_in(clk_100mhz),
-        .rst_in(sys_rst),
-        .xinput(xcoord),
-        .yinput(ycoord),
-        .zinput(zcoord),
-        .uart_data_in(uart_data_out),
-        .valid_in(uart_receive_data_valid),
-        .write_enable(1'b1),
-        .read_enable(1'b0),
-        .valid_out(cache_stored_valid),
-        .block_data_out(block_sample_data)
-    );
+    // l3_cache #(
+    //     .LENGTH(64),
+    //     .WIDTH(64),
+    //     .HEIGHT(16)
+    // ) l3_cache (
+    //     .clk_in(clk_100mhz),
+    //     .rst_in(sys_rst),
+    //     .xinput(xcoord),
+    //     .yinput(ycoord),
+    //     .zinput(zcoord),
+    //     .uart_data_in(uart_data_out),
+    //     .valid_in(uart_receive_data_valid),
+    //     .write_enable(1'b1),
+    //     .read_enable(1'b0),
+    //     .valid_out(cache_stored_valid),
+    //     .block_data_out(block_sample_data)
+    // );
 
 
 
     // Testing fixed's synthesis with BS input/outputs so it doesn't get optimized out
-    vec3 a;
-    vec3 b;
-    vec3 c;
+    vec3        ray_origin;
+    vec3        ray_direction;
+    BlockType   hit;
+    vec3        hit_norm;
+    logic       hit_valid;
+    BlockPos    ram_addr;
+    logic       ram_read_enable;
+    BlockType   ram_out;
+    logic       ram_valid;
 
-    vec3 c_pipe;
-
-    vec3_normalize normalize(
+    VoxelTraversalUnit vtu(
         .clk_in(clk_100mhz),
-        .in(a),
-        .out(c)
+        .rst_in(rst_in),
+        .ray_origin(ray_origin),
+        .ray_direction(ray_direction),
+        .hit(hit),
+        .hit_norm(hit_norm),
+        .hit_valid(hit_valid),
+        .ram_addr(ram_addr),
+        .ram_read_enable(ram_read_enable),
+        .ram_out(ram_out),
+        .ram_valid(ram_valid)
     );
-
-    always_ff @(posedge clk_100mhz) begin
-        a <= {sw, sw, sw, btn[3], sw, sw, sw} & {sw, sw, btn[1], sw, btn[2], sw, sw};
-        b <= {sw, sw, sw, btn[3], sw, sw, sw} | {sw, sw, btn[1], sw, btn[2], sw, sw};
-        
-        c_pipe <= vdot(a, b);
-        led <= {c_pipe ^ c_pipe[19:15] + c_pipe[59:40]};
-
-        // c <= fmul(a, b);
-    end
-
-    // vec3_testbench vtest(
-    //     .clk_in(clk_100mhz),
-    //     .a(a),
-    //     .b(b),
-    //     .norm(v_norm)
-    // );
 endmodule
 
 `default_nettype wire
